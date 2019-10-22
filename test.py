2c34c93f975b03a13a3a9276b55b1640a612374d
modory <modory@localhost.localdomain>
2019-10-23 06:08:04 +0800

    新文件：    test.py

---
+++ test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import csv
+import sys,getopt
+import os
+import re
+
+
+def main(argv):
+	inputfile = ''
+	comparefile = ''
+	outputfile = ''
+	try:
+		opts, args=getopt.getopt(argv,"hi:o:c:",["input=","output=","compare="])
+	except getopt.GetoptError:
+		print(1)
+		print ('test.py -i <inputfile>  -o <outputfile>  -c <comparefile>')
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt ==  '-h':
+			print ('test.py -i <inputfile> -o <outputfile>  -c <comparefile>')
+			sys.exit()
+		elif opt in ("-i", "--input"):
+			inputfile = arg
+		elif opt in ("-o", "--output"):
+			outputfile = arg
+		elif opt in ("-c", "--comare"):
+			comparefile = arg
+			print(inputfile,outputfile,comparefile)
+	if(inputfile != None):
+		csv1=open(inputfile,mode="r",encoding="utf-8")
+		reader=csv.reader(csv1)
+	if(outputfile != None):
+		csv2=open(outputfile,mode="w",encoding="utf-8")
+		writer=csv.writer(csv2)
+	if(comparefile != None):
+		csv3=open(comparefile,mode="r",encoding="utf-8")
+	csv1.close()
+	csv2.flush()
+	csv2.close()
+	csv3.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
