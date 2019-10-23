9e6503f7f49472911d9c0beb86a5f7be44edb65e
modory <modory@localhost.localdomain>
2019-10-23 15:36:14 +0800

    提交者：   modory <modory@localhost.localdomain> 	修改：      sks.py 	修改：      test.py 	删除：      mirrors.sohu.com/fedora-epel/7/x86_64/Packages/l/index.html.tmp

--- sks.py
+++ sks.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
 ## infomation
@@ -9,56 +9,46 @@ import os
 import csv
 import re
 
+
 def main(argv):
    inputfile = ''
    outputfile = ''
+   comparefile = ''
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["input=","output="])
+	opts, args = getopt.getopt(argv,"hi:o:c:",["input=","output=","compare="])
    except getopt.GetoptError:
-      print ('test.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
+	print ('test.py -i <inputfile> -o <outputfile> -c <comparefile>')
+	sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print ('test.py -i <inputfile> -o <outputfile>')
-         sys.exit()
+	   print ('test.py -i <inputfile> -o <outputfile> -c <compaerfile>')
+	   sys.exit()
       elif opt in ("-i", "--input"):
-         inputfile = arg
+	   inputfile = arg
       elif opt in ("-o", "--output"):
-         outputfile = arg
+	   outputfile = arg
+	elif opt in ("-c", "--compare"):
+	   comparefile = arg
+   print(inputfile,outputfile,comparefile)
 
-
-
-##定义列表查找字符串比较函数
-def findStr(alist,astr):
-    for substr in alist:
-        if(re.search(substr,astr)):
-            return (substr+'\n')
-
-    return astr
     
-## 定义列表
-basestr=['毛庄行政村','毛寨行政村','赵楼行政村','付楼行政村','高小庙行政村','卢寨行政村','王拱行政村','刘营行政村','徐庄行政村','大王庄行政村','朱楼行政村','谢庄行政村','王大庄行政村','从楼行政村','崔堂行政村','李楼行政村','李黄楼行政村','杨庙行政村','大刘庄行政村','大郭庄行政村','洼吴行政村','张白庄行政村','薛庄行政村','陶店行政村','丁村行政村','毛庄行政村','毛寨行政村','赵楼行政村','付楼行政村','高小庙行政村','卢寨行政村','王拱行政村','刘营行政村','徐庄行政村','大王庄行政村','朱楼行政村','谢庄行政村','王大庄行政村','从楼行政村','崔堂行政村','李楼行政村','李黄楼行政村','杨庙行政村','大刘庄行政村','大郭庄行政村','洼吴行政村','张白庄行政村','薛庄行政村','陶店行政村','丁村行政村']
-list=[]
+   list=[]
 ## 从文件中取出一行  
 ##如果不是二进制形式打开文件，encoding='unicode_escape'
-csvFile=open(inputfile,mode="r",encoding='gbk')
-reader=csv.reader(csvFile)
-for item in reader:  
-    if(item != None):
-        addr=findStr(basestr,item[14])
-        item[14]=addr
-        list.append(item)
-    else:
-        continue
-
-csvFile2=open(outputfile,mode="w",newline='')
-writer=csv.writer(csvFile2)
-for index in list:
-    writer.writerow(index)
+   if(inputfile != None ):
+	csvFile=open(inputfile,mode="r",encoding='utf-8')
+	reader=csv.reader(csvFile)
+   if(outputfile != None):
+	csvFile2=open(outputfile,mode="w",newline='')
+	writer=csv.writer(csvFile2)
+   if(comparefile != None):
+	comFile=open(comparefile,mode="r",encoding='utf-8')
+	reader1=csv.reader(comFile)
 ## 退出
-csvFile2.flush()
-csvFile2.close()
-csvFile.close()
+   csvFile2.flush()
+   csvFile2.close()
+   comFile.close()
+   csvFile.close()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -27,7 +27,6 @@ def main(argv):
 			outputfile = arg
 		elif opt in ("-c", "--comare"):
 			comparefile = arg
-			print(inputfile,outputfile,comparefile)
 	if(inputfile != None):
 		csv1=open(inputfile,mode="r",encoding="utf-8")
 		reader=csv.reader(csv1)
@@ -36,6 +35,15 @@ def main(argv):
 		writer=csv.writer(csv2)
 	if(comparefile != None):
 		csv3=open(comparefile,mode="r",encoding="utf-8")
+		reader2=csv.reader(csv3)
+	
+	for line in reader:
+			for row in reader2:
+			if(line[1] == row[5]):
+				line.append(row[13])
+				line.append(row[14])
+				line.append(row[15])
+				writer.writerow(line)
 	csv1.close()
 	csv2.flush()
 	csv2.close()
